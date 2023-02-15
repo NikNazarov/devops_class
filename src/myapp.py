273359533dbcf277905b56d39baf203f47009993
NikNazarov <nazarov.nik.an@gmail.com>
2023-02-16 00:01:46 +0700

    initial

---
+++ src/myapp.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import PyQt5
+import matplotlib
+
+
+def return_hello():
+    return "Hellow world!"
+
+if __name__  == "__main__":
+    print("It's Working!!!")
---
+++ tests/test_app.py
@@ -0,0 +1,6 @@
+from myapp import return_hello
+
+def test_hello():
+    assert return_hello() == "Hellow world!"
+
+
